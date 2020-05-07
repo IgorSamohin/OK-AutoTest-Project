06ca643f38692553ebe9768e23ce2a70a02af724
IgorSamohin <57264167+IgorSamohin1@users.noreply.github.com>
2020-05-07 20:03:42 +0300

    Adding Base Page class

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -1,4 +1,29 @@
 package Pages;
 
-public class BasePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public abstract class BasePage {
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    protected void type(String text, By locator) {
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    protected void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    protected void clickBy(By locator, int xOffSet, int yOffSet) {
+        WebElement webElement = driver.findElement(locator);
+        Actions builder = new Actions(driver);
+        builder.moveToElement(webElement, xOffSet, yOffSet).click().build().perform();
+    }
 }
