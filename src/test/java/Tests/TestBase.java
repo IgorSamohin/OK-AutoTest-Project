82c452f3d31e3a5fac5c130bb2825b0aeb867349
Resst <55293545+Resst@users.noreply.github.com>
2020-05-07 19:24:26 +0300

    some test creation

--- src/test/java/Tests/TestBase.java
+++ src/test/java/Tests/TestBase.java
@@ -1,4 +1,12 @@
 package Tests;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestBase {
+
+    @Test
+    public void someTest(){
+        Assertions.assertTrue(2 + 2 == 5);
+    }
 }
