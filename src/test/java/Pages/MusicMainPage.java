81178ed201d8017f058c38ebb4c5ae3566133446
IgorSamohin <57264167+IgorSamohin1@users.noreply.github.com>
2020-05-16 14:10:13 +0300

    Changing all locators to 'By' view

--- src/test/java/Pages/MusicMainPage.java
+++ src/test/java/Pages/MusicMainPage.java
@@ -4,12 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class MusicMainPage extends OkPage{
-    private final String MY_MUSIC_BUTTON_XPATH = "//*[@class = 'content_menu __60o4ct']//*[@data-l = 't,library']";
+    private final By MY_MUSIC_BUTTON_XPATH = By.xpath("//*[@class = 'content_menu __60o4ct']//*[@data-l = 't,library']");
     public MusicMainPage(WebDriver driver) {
         super(driver);
     }
 
     public void clickOnMyMusic() { //TODO add try catch
-        click(By.xpath(MY_MUSIC_BUTTON_XPATH));
+        click(MY_MUSIC_BUTTON_XPATH);
     }
 }
--- src/test/java/Pages/UserMainPage.java
+++ src/test/java/Pages/UserMainPage.java
@@ -1,22 +1,30 @@
 package Pages;
 
+import Pages.Groups.GroupMainPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 
 public class UserMainPage extends OkPage {
-    private static final String USER_PROPERTIES_WINDOW_XPATH = "//*[@class='ucard-mini toolbar_ucard js-toolbar-menu']";
-    private static final String USER_LOGOUT_XPATH = "//*[@data-l='t,logoutCurrentUser']";
-    private static final String CONFIRM_EXIT = "//*[@data-l='t,confirm']";
+    private static final By USER_PROPERTIES_WINDOW_XPATH = By.xpath("//*[@class='ucard-mini toolbar_ucard js-toolbar-menu']");
+    private static final By USER_LOGOUT_XPATH = By.xpath("//*[@data-l='t,logoutCurrentUser']");
+    private static final By CONFIRM_EXIT = By.xpath("//*[@data-l='t,confirm']");
+
+    private static final By GROUP_LOCATOR = By.xpath("//*[contains(@data-l, 't,userAltGroup')]");
 
     public UserMainPage(WebDriver driver) {
         super(driver);
     }
 
     public LoginPage doLogout() {
-        driver.findElement(By.xpath(USER_PROPERTIES_WINDOW_XPATH)).click();
-        driver.findElement(By.xpath(USER_LOGOUT_XPATH)).click();
-        driver.findElement(By.xpath(CONFIRM_EXIT)).click();
+        driver.findElement(USER_PROPERTIES_WINDOW_XPATH).click();
+        driver.findElement(USER_LOGOUT_XPATH).click();
+        driver.findElement(CONFIRM_EXIT).click();
         return (new LoginPage(driver));
+    }
+
+    public GroupMainPage goToGroupPage(){
+        click(GROUP_LOCATOR);
+        return (new GroupMainPage(driver));
     }
 }
