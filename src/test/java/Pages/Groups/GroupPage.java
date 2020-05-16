68905a2fe7fa7c628c6ea64455a0806bad7fe479
IgorSamohin <57264167+IgorSamohin1@users.noreply.github.com>
2020-05-16 15:32:08 +0300

    Adding method, which can check name, description and type of the group

--- src/test/java/Pages/Groups/GroupPage.java
+++ src/test/java/Pages/Groups/GroupPage.java
@@ -9,6 +9,9 @@ public class GroupPage extends OkPage {
     private final By DELETE_OPTION_LOCATOR = By.xpath("//*[contains(@class, 'svg-ic svg-ico_trash_16 null')]/..");
     private final By DELETE_SUBMIT_BUTTON_LOCATOR = By.xpath("//*[contains(@data-l, 't,confirm')]");
 
+    private final By GROUP_NAME_LOCATOR =  By.xpath("//*[contains(@class, 'group-name_t')]");
+    private final By GROUP_DESCRIPTION_LOCATOR =  By.xpath("//*[contains(@data-module, 'GroupInfoPanel')]");
+    private final By GROUP_TYPE_LOCATOR =  By.xpath("//*[contains(@class, 'group-name_info')]");
 
     public GroupPage(WebDriver driver) {
         super(driver);
@@ -19,5 +22,17 @@ public class GroupPage extends OkPage {
         click(DELETE_OPTION_LOCATOR);
         click(DELETE_SUBMIT_BUTTON_LOCATOR);
         return (new GroupMainPage(driver));
+    }
+
+    public String getGroupName(){
+        return driver.findElement(GROUP_NAME_LOCATOR).getText();
+    }
+
+    public String getGroupDescription(){
+        return driver.findElement(GROUP_DESCRIPTION_LOCATOR).getText();
+    }
+
+    public boolean isPublicGroup(){
+        return (driver.findElement(GROUP_TYPE_LOCATOR).getText().equals("Публичная страница"));
     }
 }
