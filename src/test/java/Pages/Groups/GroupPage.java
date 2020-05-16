eb9aa2b46cce1fd9f401d6eab17ff082e0a3d3a9
IgorSamohin <57264167+IgorSamohin1@users.noreply.github.com>
2020-05-16 14:09:35 +0300

    Adding Group page with delete method

---
+++ src/test/java/Pages/Groups/GroupPage.java
@@ -0,0 +1,23 @@
+package Pages.Groups;
+
+import Pages.OkPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class GroupPage extends OkPage {
+    private final By GROUP_FAST_PREFERENCES_BUTTON_LOCATOR = By.xpath("//*[contains(@class, 'u-menu_a toggle-dropdown')]");
+    private final By DELETE_OPTION_LOCATOR = By.xpath("//*[contains(@class, 'svg-ic svg-ico_trash_16 null')]/..");
+    private final By DELETE_SUBMIT_BUTTON_LOCATOR = By.xpath("//*[contains(@data-l, 't,confirm')]");
+
+
+    public GroupPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public GroupPage deleteGroup(){
+        click(GROUP_FAST_PREFERENCES_BUTTON_LOCATOR);
+        click(DELETE_OPTION_LOCATOR);
+        click(DELETE_SUBMIT_BUTTON_LOCATOR);
+        return (new GroupPage(driver));
+    }
+}
